fa41a16294bc519c691723862b95002a56f267ec
merini <gutomerini@gmail.com>
2019-03-07 18:58:50 -0300

    Refactors, Hello World example

--- routes/users.js
+++ routes/users.js
@@ -2,8 +2,8 @@ var express = require('express');
 var router = express.Router();
 
 /* GET users listing. */
-router.get('/', function(req, res, next) {
-  res.send('Hello World!');
+router.get('/', (req, res, next)  =>{
+  res.send('Hello world');
 });
 
 module.exports = router;

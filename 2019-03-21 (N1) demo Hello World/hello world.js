fedb5bc87ae4168c84117b68ad9d84a8807e2972
merini <gutomerini@gmail.com>
2019-03-21 20:47:36 -0300

    Hello world frontend

--- 2019-03-21 (N1) demo Hello World/hello world.js
+++ 2019-03-21 (N1) demo Hello World/hello world.js
@@ -7,3 +7,6 @@ router.get('/', (req, res, next) => {
 });
 
 module.exports = router;
+
+// com esse código, ao acessar http://localhost:3000, veremos que nosso backend está rodando 
+// e uma mensagem de hello world será mostrada
